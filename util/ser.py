b41fcec31e23048efe4a85d2b905cc035b8eab4d
oeitam <oeitam@gmail.com>
2016-10-12 16:11:14 +0100

    840 crush, update ser.py

--- util/ser.py
+++ util/ser.py
@@ -24,8 +24,44 @@ def serializeProgram(program):
 
 #############################
 
+# parameters
+
+# 1: file name to serialize
+# 2: target of serialization
+#    oracle
+#    test
+#    3: type: security
+#             correctness
+#             crush
+#    4: target biold: 900
+#                     840
+#                     ...
+
+if len(sys.argv) < 2 :
+    print "bad input 1"
+    exit()
+
 filename = sys.argv[1]
-print filename
+print "filename: %s" % filename
+
+oracle = 0
+test = 0
+typ = ""
+target = 0
+
+if sys.argv[2] == "oracle":
+    oracle = 1
+elif sys.argv[2] == "test":
+    test = 1
+    typ = sys.argv[3]
+    target = sys.argv[4]
+else:
+    print "baf input 2"
+    exit()
+
+    
+
+    
 
 
 progs1 = []
@@ -47,5 +83,38 @@ for line in f:
 
 f.close()
 
-for p in progs1:
-    print p
+if oracle == 1 :
+    print "{ \"arguments\":[\"6300\",\"admin\"], \"programs\":["
+    l = len(progs1)
+    #print l
+    for i in range(0,l):
+        #print i
+        print "\"" + str(progs1[i]) + "\"" ,
+        if ((l > 1) and (i < (l-1))) :
+            print ","
+    print "] }"
+    exit()
+
+if test == 1 :
+    print "{"
+    print "    \"type\": \"" + typ + "\","
+    print "    \"target_team\": " + str(target) + ","
+    print "    \"arguments\": {"
+    print "         \"argv\": [\"%PORT%\"],"
+    print "         \"base64\": false"
+    print "    },"
+    print "    \"programs\": ["
+    l = len(progs1)
+    #print l
+    for i in range(0,l):
+        #print i
+        # print "\"" + str(progs1[i]) + "\"" ,
+        print "         {\"program\":" + str(progs1[i]) +  "\", \"base64\": false}" ,
+        if ((l > 1) and (i < (l-1))) :
+            print ","
+
+    print "    ]"
+    print "}"
+    exit()
+
+
